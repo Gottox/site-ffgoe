7bcafb0e05e327cae89027717f12d304db8feebb
Daniel Rippen <rippendaniel@gmail.com>
2018-06-25 08:35:42 +0200

    Update to v2017.1.8

--- site.mk
+++ site.mk
@@ -95,7 +95,7 @@ endif
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := v2017.1.7
+DEFAULT_GLUON_RELEASE := v2017.1.8
 
 ##	GLUON_RELEASE
 #		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
